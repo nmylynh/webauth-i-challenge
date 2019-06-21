d55d810cb16d6fb68288ba4eab8e5b938a6a6fdd
Mylynh Nguyen <34496865+nmylynh@users.noreply.github.com>
2019-06-21 06:08:51 -0700

    created users seed

--- database/migrations/20190621060001_users_table.js
+++ database/migrations/20190621060001_users_table.js
@@ -13,6 +13,6 @@ exports.up = function(knex, Promise) {
       })
   };
   
-  exports.down = function(knex, Promise) {
+exports.down = function(knex, Promise) {
     return knex.schema.dropTableIfExists('users')
-  };
+};
